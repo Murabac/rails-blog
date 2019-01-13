39e99f0661c23d5f30f00b8381d579e0045015c0
Murabac <Mur7bac@gmail.com>
2019-01-13 14:19:49 +0300

    Form-Basedd workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,48 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper #this helper lets us verify what the user has sent to us
+
   #This is what the app will send back when a user requests http://localhost:3000/articles/
     def index
         @articles = Article.all
     end
 
-    #Show action method
+    #New Show article action method
     def show
-      @article = Article.find(params[:id]);
+      @article = Article.find(params[:id])
     end
+
+    #New post Action Method
+    def new
+      @article = Article.new
+    end
+
+    #From that Creates New post
+    def create
+      @article = Article.new(article_params)
+      @article.save
+      flash.notice = "Article '#{@article.title}' Created!"
+      redirect_to article_path(@article)
+    end
+
+    #Destory or Delete a Post
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash.notice = "Article '#{@article.title}' Deleted!"
+      redirect_to action: "index"
+    end
+
+    #Edit postAction Method
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    #Update a post
+    def update
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article)
+    end
+
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,6 @@
 module ArticlesHelper
+  #this to aviod the rails error of forbidden atrripute
+  def article_params
+  params.require(:article).permit(:title, :body)
+  end
 end
