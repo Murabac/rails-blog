1be6204e966685a9f41fb36d3e095742417a3436
Murabac <Mur7bac@gmail.com>
2019-01-14 10:32:24 +0300

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,9 @@ class ArticlesController < ApplicationController
     #New Show article action method
     def show
       @article = Article.find(params[:id])
+      @comment = Comment.new
+      @comment.article_id = @article.id
+
     end
 
     #New post Action Method
