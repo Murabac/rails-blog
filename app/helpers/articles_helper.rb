ff2ac4fdda1add418eedc55fcbccf3f0f3253a4f
Murabac <Mur7bac@gmail.com>
2019-01-14 18:10:10 +0300

    Added few gems

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   #this to aviod the rails error of forbidden atrripute
   def article_params
-      params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
     end
 end
