563993d4ef417001544fe844ebf859a0affbbfb4
Murabac <Mur7bac@gmail.com>
2019-01-14 13:27:10 +0300

    Tagging Feature Almost completed the deleting functionality is missing

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   #this to aviod the rails error of forbidden atrripute
   def article_params
-  params.require(:article).permit(:title, :body)
-  end
+      params.require(:article).permit(:title, :body, :tag_list)
+    end
 end
